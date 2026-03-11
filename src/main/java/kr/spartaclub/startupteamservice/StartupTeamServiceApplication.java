c32558778c4627ada2b32e3e51dc7159e8486a02
Jeong Jihun <doksakim7@naver.com>
2026-03-11 12:11:48 +0900

    feat: 첫 커밋 - 초기 세팅

---
+++ src/main/java/kr/spartaclub/startupteamservice/StartupTeamServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.spartaclub.startupteamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupTeamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartupTeamServiceApplication.class, args);
+    }
+
+}
